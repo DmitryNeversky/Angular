7372e059da6387ea3fe3863eaf79efb1ebca9aad
Koshey <shdeemon@mail.ru>
2021-05-04 06:38:24 +0300

    v0.0.9

---
+++ src/app/templates/admin/items/item/item.component.ts
@@ -0,0 +1,118 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {faCheck, faChevronDown, faTimes} from "@fortawesome/free-solid-svg-icons";
+import {Subcategory} from "../../../../models/subcategory";
+import {Item} from "../../../../models/item";
+import {NgForm} from "@angular/forms";
+import {ItemService} from "../../../../services/item.service";
+import {SubcategoryService} from "../../../../services/subcategory.service";
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  public icons = {times: faTimes, check: faCheck, open: faChevronDown}
+
+  @Input()
+  public item: Item;
+
+  @Output()
+  private itemEvent = new EventEmitter<Item>();
+
+  public subCategories: Subcategory[];
+  public loadImages = [];
+  private dataTransfer: DataTransfer;
+
+  constructor(private itemService: ItemService, private subCategoryService: SubcategoryService) { }
+
+  ngOnInit(): void {
+    this.dataTransfer = new DataTransfer();
+    this.subCategoryService.getAll().subscribe(response => {
+      this.subCategories = response;
+    }, error => console.log(error));
+  }
+
+  load(event) {
+
+    let component = this;
+
+    if (event.target.files && event.target.files[0]) {
+
+      for(const file of event.target.files){
+
+        let ext = file.name.match(/\.([^\.]+)$/)[1];
+
+        switch (ext) {
+          case 'jpg':
+          case 'jpeg':
+          case 'png':
+            break;
+          default:
+            continue;
+        }
+
+        this.dataTransfer.items.add(file);
+
+        let reader = new FileReader();
+
+        reader.onload = function (e){
+          component.loadImages.push(e.target.result);
+        }
+
+        reader.readAsDataURL(file); // convert to base64 string
+      }
+
+      event.target.files = this.dataTransfer.files;
+    }
+  }
+
+  removeImage(event){
+    this.dataTransfer.items.remove(event.target)
+    event.target.remove()
+  }
+
+  public removeImagesList: string[] = [];
+
+  remImage(event, image){
+    if(event.target.hasAttribute('remove')) {
+      event.target.removeAttribute('remove');
+      this.removeImagesList = this.removeImagesList.filter(x => x != image);
+      event.target.style.opacity = '1';
+    } else {
+      event.target.setAttribute('remove', null);
+      this.removeImagesList.push(image);
+      event.target.style.opacity = '0.5';
+    }
+  }
+
+  updateItem(form: NgForm) {
+
+    let formData = new FormData();
+
+    formData.append('name', form.value.name);
+    formData.append('description', form.value.description);
+    formData.append('price', form.value.price);
+    formData.append('count', form.value.count);
+    formData.append('subCategory', form.value.subCategory);
+    this.removeImagesList.forEach(x => formData.append('removeImages', x));
+    for (let i = 0; i < this.dataTransfer.files.length; i++)
+      formData.append('addImages', this.dataTransfer.files[i]);
+
+    this.itemService.update(form.value.id, formData).subscribe((response: Item) => {
+      console.log(response);
+      // this.items.push(response);
+    }, error => console.log(error));
+  }
+
+  openItem(event){
+    event.target.style.transform = 'rotate(180deg)';
+  }
+
+  deleteItem(item: Item) {
+    this.itemService.delete(item.id).subscribe(() => {
+      // this.items.splice(this.items.indexOf(item), 1);
+    }, error => console.log(error));
+  }
+}
