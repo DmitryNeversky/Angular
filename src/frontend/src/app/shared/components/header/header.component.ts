62004db2ba68bb5639f55e4c6f94847a3f49e995
Koshey <shdeemon@mail.ru>
2021-06-05 22:20:42 +0300

    v0.8.3

--- src/frontend/src/app/shared/components/header/header.component.ts
+++ src/frontend/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +7,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild('header')
+  private headerElement: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
 
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    if(window.scrollY > 70){
+      this.headerElement.nativeElement.style.padding = '1% 0';
+    } else if (window.scrollY < 70){
+      this.headerElement.nativeElement.style.padding = '2% 0';
+    }
   }
 
 }
